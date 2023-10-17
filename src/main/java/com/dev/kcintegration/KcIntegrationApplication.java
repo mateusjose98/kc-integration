6a6729c9590d3abee1e620485072c2f1c10e3b41
josemateus <programadorjavante@gmail.com>
2023-10-17 20:10:24 -0300

    Initial commit

---
+++ src/main/java/com/dev/kcintegration/KcIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dev.kcintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/kcintegration/KcIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.kcintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
